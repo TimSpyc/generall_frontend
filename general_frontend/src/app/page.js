92aa511a377a02558bd950dac50f92cc6fc604c1
julianlaibach <53005042+julianlaibach@users.noreply.github.com>
2023-11-14 12:54:32 +0000

    doubled asset

--- general_frontend/src/app/page.js
+++ general_frontend/src/app/page.js
@@ -75,6 +75,71 @@ const Home = () => {
             <Input link="posts" name="user.test.1"/>
           </View>
         </Asset>
+        <Asset name="custom-asset-test-1">
+          <View
+            type={['default']} 
+            api={{
+              users: {url: "https://dummyjson.com/users"},
+              posts: {url: "https://dummyjson.com/posts"}
+            }}>
+            <Input
+              name="user.test.1"
+              link="users" 
+              placeholder="test 1"
+              actions={{
+                edit: "edit", 
+                filters: true
+              }}/>
+            <Input 
+              name="user.test.2"
+              link="users" 
+              placeholder="test 2"
+              actions={{
+                edit: "edit", 
+                filters: true
+              }}/>
+            <Input 
+              name="user.test.3"
+              link="users" 
+              placeholder="test 3"
+              actions={{
+                edit: "edit.users", 
+                filters: true
+              }}/>
+            <Input 
+              name="user.test.10"
+              link="users" 
+              placeholder="test 3"
+              actions={{
+                edit: "edit.users", 
+                filters: true
+              }}/>
+          </View>
+          <View
+            type={['detail']} 
+            api={{
+              users: {url: "https://dummyjson.com/users"},
+              posts: {url: "https://dummyjson.com/posts"}
+            }}>
+            <Input link="posts" name="user.test.1"/>
+          </View>
+          <View
+            type={['edit']} 
+            api={{
+              users: {url: "https://dummyjson.com/users"},
+              posts: {url: "https://dummyjson.com/posts"}
+            }}>
+            <Input link="posts" name="user.test.1"/>
+          </View>
+          <View
+            type={['filters']} 
+            api={{
+              users: {url: "https://dummyjson.com/users"},
+              posts: {url: "https://dummyjson.com/posts"}
+            }}>
+            <Input link="posts" name="user.test.1"/>
+          </View>
+        </Asset>
       </GridLayout>
     </main>
   )
